70c9496634b636fc47c19386557e1865846643ca
ganesh2425 <saahoganesh023@gmail.com>
2021-02-28 11:17:46 +0530

    ok

---
+++ Functions/challenge.js
@@ -0,0 +1,5 @@
+let str = "New Year 2020";
+
+let newStr = str.replaceAll("20","21");
+
+console.log(newStr);
---
+++ obj2.js
@@ -0,0 +1,7 @@
+let value = 10;
+
+if (value = 0){
+    console.log(true);
+} else {
+    console.log(false);
+}
