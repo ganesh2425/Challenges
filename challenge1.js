fea900615833081459aa797492b242d38360a360
ganesh2425 <saahoganesh023@gmail.com>
2021-03-02 06:17:22 +0530

    ok

---
+++ challenge1.js
@@ -0,0 +1,9 @@
+let object = { name : "Priyanka"};
+
+let value = null;
+
+if (typeof null === typeof object){
+    console.log(typeof null);
+} else {
+    console.log(false);
+}
---
+++ challenge2.js
@@ -0,0 +1,7 @@
+let detail = [];
+detail["first"] = "Kummara"
+detail["last"] = "Ganesh";
+detail["age"] = 21;
+
+console.log(detail.length);
+console.log(detail[0]);
